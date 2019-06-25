d8bbf92ee23a1836b9a4efba326419dcd5ba600b
dominic montelongo and joshua.horgen <joshua.horgen@>
2019-06-25 16:09:25 -0700

    Add initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { template } from './template';
 import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
+  console.log('Jquery is working!');
   $('#template-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
